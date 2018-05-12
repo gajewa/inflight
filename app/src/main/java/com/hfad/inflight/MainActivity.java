cd6f4b360fa217845e4da2e4b7b3519da6b60511
Blaszczu <krzysb41@gmail.com>
2018-05-12 14:55:18 +0200

    przyciski

--- app/src/main/java/com/hfad/inflight/MainActivity.java
+++ app/src/main/java/com/hfad/inflight/MainActivity.java
@@ -1,13 +1,43 @@
 package com.hfad.inflight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button inFlightButton;
+    private Button groundbutton;
+    private Button ofertybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inFlightButton = (Button) findViewById(R.id.inflightButton);
+        inFlightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("idz do inflight");
+            }
+        });
+        groundbutton = (Button) findViewById(R.id.groundButton);
+        groundbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent k = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(k);
+            }
+        });
+        ofertybutton = (Button) findViewById(R.id.ofertyButton);
+        ofertybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("idz do ofert");
+            }
+        });
     }
 }
